8b430627c096c3a274040110ce4509531b8f0cfb
Shengwen Yang <yangs16@cnenyangs16mbp2.corp.emc.com>
2012-12-17 19:40:39 +0800

    add/remove/modify some UDFs

--- src/modules/newplda/newplda.hpp
+++ src/modules/newplda/newplda.hpp
@@ -6,16 +6,15 @@
  *
  *//* ----------------------------------------------------------------------- */
 
+DECLARE_UDF(newplda, newplda_random_assign)
 DECLARE_UDF(newplda, newplda_gibbs_train)
-
 DECLARE_UDF(newplda, newplda_gibbs_pred)
 
-DECLARE_UDF(newplda, newplda_random_assign)
-
-DECLARE_UDF(newplda, newplda_count_word_topic)
-
-DECLARE_UDF(newplda, newplda_array_add)
-
-DECLARE_UDF(newplda, newplda_array_fold_add)
+DECLARE_UDF(newplda, newplda_count_topic_sfunc)
+DECLARE_UDF(newplda, newplda_count_topic_prefunc)
+DECLARE_UDF(newplda, newplda_count_topic_ffunc)
 
 DECLARE_UDF(newplda, newplda_first)
+
+DECLARE_UDF(newplda, newplda_transpose)
+DECLARE_SR_UDF(newplda, newplda_unnest)
